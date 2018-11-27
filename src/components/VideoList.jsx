77e4a7cf2258634d78d5dfdf28bb91f5fa194632
velezjose <velezq.jose@gmail.com>
2018-11-26 16:08:34 -0800

    Make VideoList render videos from VideoListEntry

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,10 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.jsx';
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map(video =>
+      <VideoListEntry video={video} />
+    )}  
   </div>
 );
 
@@ -14,6 +14,11 @@ VideoList.propTypes = {
   videos: React.PropTypes.array.isRequired
 };
 
+
+
+//  <div><h5><em>videoListEntry</em> view goes here</h5></div>
+
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+ 
