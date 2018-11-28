a4c08d76346eaf698cadd7eea184354f9eda4a38
velezjose <velezq.jose@gmail.com>
2018-11-27 16:43:52 -0800

    Pass in searchYouTube as a prop of App

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
 import exampleVideoData from './data/exampleVideoData.js';
+import searchYouTube from './lib/searchYouTube.js';
 
-ReactDOM.render(<App props={exampleVideoData} />, document.getElementById('app'));
+ReactDOM.render(<App searchYouTube={searchYouTube} />, document.getElementById('app'));
